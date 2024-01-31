86977a5582500f3dc428295b0b7f836aa1a4eee4
David Pedersen <limero@me.com>
2024-01-31 14:35:25 +0800

    Add individual FindURLs test cases

--- find_test.go
+++ find_test.go
@@ -4,16 +4,33 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestFindURLs(t *testing.T) {
-	in := `
+	t.Run("find in text", func(t *testing.T) {
+		in := `
 	lorem ipsum https://example.com ipsum lorem https://example.com/test?a=123
 	`
-	out := FindURLs(in)
-	assert.Len(t, out, 2)
-	assert.Equal(t, "https://example.com", out[0].String())
-	assert.Equal(t, "https://example.com/test?a=123", out[1].String())
+		out := FindURLs(in)
+		assert.Len(t, out, 2)
+		assert.Equal(t, "https://example.com", out[0].String())
+		assert.Equal(t, "https://example.com/test?a=123", out[1].String())
+	})
+
+	t.Run("find individual", func(t *testing.T) {
+		tt := map[string]string{
+			"<a href=\"https://example.com\">":                        "https://example.com",
+			"https://example.com/?utm_source=rss&amp;utm_medium=feed": "https://example.com/?utm_source=rss&amp;utm_medium=feed",
+			"https://example.com/tag/Hello+World/":                    "https://example.com/tag/Hello+World",
+		}
+
+		for in, expected := range tt {
+			out := FindURLs(in)
+			require.Len(t, out, 1)
+			assert.Equal(t, expected, out[0].String())
+		}
+	})
 }
 
 func TestFindURLsWithPos(t *testing.T) {
